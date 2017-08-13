41bd797621f8ff5a85f7baf9c2d5950bc6175db1
sunlong <2013855675@qq.com>
2017-08-13 14:34:46 +0800

    fix  bug

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Service\ValidatePermission;
 use Dingo\Api\Routing\Helpers;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Access\AuthorizationException;
@@ -9,63 +10,6 @@ use Auth;
 
 class BaseController extends Controller
 {
-    use Helpers;
+    use Helpers,ValidatePermission;
 
-    /**
-     * Get the guard to be used during authentication.
-     * 用户组（在 Laravel5.3 中对于多组用户有更加完善的支持，可以有多组用户系统，比方说前台、后台各有一组用户系统。）
-     * @return \Illuminate\Contracts\Auth\StatefulGuard
-     */
-    protected function guard()
-    {
-        return Auth::guard('web');
-    }
-
-    protected function validatePermission($permissions)
-    {
-        if (!$this->guard()->check() || !$this->guard()->user()->may($permissions)) {
-            throw new AuthorizationException();
-        }
-        return true;
-    }
-
-    protected function allowCreateTask()
-    {
-        return $this->validatePermission('admin.create_task');
-    }
-
-    protected function allowAuditTask()
-    {
-        return $this->validatePermission('admin.audit_task');
-    }
-
-    protected function allowDeleteTask()
-    {
-        return $this->validatePermission('admin.delete_task');
-    }
-
-    protected function allowRestoreTask()
-    {
-        return $this->allowDeleteTask();
-    }
-
-    protected function allowAllotTask()
-    {
-        return $this->validatePermission('admin.add_person_liable');
-    }
-
-    protected function allowSubmitTask()
-    {
-        return $this->validatePermission('admin.submit_task');
-    }
-
-    protected function allowScore()
-    {
-        return $this->validatePermission('admin.quality_assessment');
-    }
-
-    protected function allowUpdateTask()
-    {
-        return $this->validatePermission('admin.edit_task');
-    }
 }
--- app/Service/ValidatePermission.php
+++ app/Service/ValidatePermission.php
@@ -1,4 +1,8 @@
 <?php
+namespace App\Service;
+
+use Illuminate\Auth\Access\AuthorizationException;
+
 /**
  * Created by PhpStorm.
  * User: 孙龙
@@ -6,3 +10,63 @@
  * Time: 14:21
  */
 
+trait ValidatePermission
+{
+    /**
+     * Get the guard to be used during authentication.
+     * 用户组（在 Laravel5.3 中对于多组用户有更加完善的支持，可以有多组用户系统，比方说前台、后台各有一组用户系统。）
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard('web');
+    }
+
+    protected function validatePermission($permissions)
+    {
+        if (!$this->guard()->check() || !$this->guard()->user()->may($permissions)) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    protected function allowCreateTask()
+    {
+        return $this->validatePermission('admin.create_task');
+    }
+
+    protected function allowAuditTask()
+    {
+        return $this->validatePermission('admin.audit_task');
+    }
+
+    protected function allowDeleteTask()
+    {
+        return $this->validatePermission('admin.delete_task');
+    }
+
+    protected function allowRestoreTask()
+    {
+        return $this->allowDeleteTask();
+    }
+
+    protected function allowAllotTask()
+    {
+        return $this->validatePermission('admin.add_person_liable');
+    }
+
+    protected function allowSubmitTask()
+    {
+        return $this->validatePermission('admin.submit_task');
+    }
+
+    protected function allowScore()
+    {
+        return $this->validatePermission('admin.quality_assessment');
+    }
+
+    protected function allowUpdateTask()
+    {
+        return $this->validatePermission('admin.edit_task');
+    }
+}
