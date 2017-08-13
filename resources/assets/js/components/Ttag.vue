23a801518a631d7eb8e97e17f0e37c457349ee3f
tianyang <1506125048@qq.com>
2017-08-13 16:23:02 +0800

    忽略生成文件

--- resources/assets/js/components/Ttag.vue
+++ resources/assets/js/components/Ttag.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="t-tag">
-    <div contenteditable="true" class="content">
+  <div class="t-tag" :canhover="!contenteditable">
+    <div :class="{'user_select_none': !contenteditable}" @blur="$emit('update', $event.target.innerHTML), contenteditable = false" @dblclick="contenteditable = true" ref="editableDIv" :contenteditable="contenteditable" class="content">
       {{content}}
     </div>
-    <span @click="$emit('on-close')" class="colse">+</span>
+    <div class="option">
+      <span @click="contenteditable = true" class="edit"><i class="el-icon-edit"></i></span>
+      <span @click="$emit('on-close')" class="colse"><i class="el-icon-delete"></i></span>
+    </div>
   </div>
 </template>
 
@@ -11,20 +14,50 @@
 export default {
   props: {
     content: String
+  },
+  data () {
+    return {
+      contenteditable: false
+    }
+  },
+  watch: {
+    contenteditable (newVal) {
+      if(newVal){
+        setTimeout(() => {
+          this.$refs['editableDIv'].focus();
+        },70)
+      }
+    }
+  },
+  mounted () {
+    this.$refs['editableDIv'].onkeydown = (event) => {
+      if(event.keyCode == 13){
+        return false
+      }
+      return true;
+    }
   }
 }
 </script>
 
 <style lang="css">
+.user_select_none{
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
 .t-tag{
   position: relative;
   display: inline-block;
   margin: 0 10px 8px 0;
 }
+.t-tag[canhover=true]:hover>.option{display: block;}
 .t-tag>.content{
   display: inline-block;
   background-color: #20a0ff;
-  border-color: #20a0ff;
   padding: 6px 8px;
   border-radius: 4px;
   width: auto;
@@ -32,23 +65,45 @@ export default {
   color: #fff;
   font-size: 14px;
   outline: none;
+  border: 3px #20a0ff solid;
+  transition: all .3s;
 }
-.t-tag>.colse{
+.t-tag>.content:focus{
+  border-style: dashed;
+  border-color: #1d86d4;
+  background-color: #e4ecf3;
+  color: #333;
+
+}
+.option{
   position: absolute;
-  top: -6px;
-  right: -7px;
-  display: inline-block;
-  width: 15px;
-  height: 15px;
-  text-align: center;
-  line-height: 15px;
-  font-weight: bold;
-  transform: rotate(45deg);
-  background-color: #ff4949;
-  color: #fff;
-  border-radius: 100px;
-  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgb(255, 255, 255);
+  border-radius: 3px;
   display: none;
 }
-.t-tag:hover>.colse{display: inline-block;}
+.option>span{
+  position: absolute;
+  top: 0;
+  line-height: 37px;
+  text-align: center;
+  display: inline-block;
+  width: 50%;
+  cursor: pointer;
+  color: #999;
+}
+.option>span:hover{
+  background-color: #f1f1f1;
+}
+.option>span.edit{
+  left: 0;
+  border-right: 1px solid #efefef;
+}
+.option>span.close{
+  right: 0;
+}
+
 </style>
--- routes/api.php
+++ routes/api.php
@@ -60,11 +60,11 @@ $api->group(['middleware' => 'auth:web'], function ($api) {
         $api->post('create_assess', 'AssessController@store');
         $api->post('update_assess/{assess_id}', 'AssessController@update');
         $api->get('delete_assess/{assess_id}', 'AssessController@delete');
-        // 学院
+        // 对口科室
         $api->post('create_departments', 'DepartmentController@store');
         $api->post('update_departments/{department_id}', 'DepartmentController@update');
         $api->get('delete_departments/{department_id}', 'DepartmentController@delete');
-        // 对口科室
+        // 学院
         $api->post('create_colleges', 'CollegeController@store');
         $api->post('update_colleges/{college_id}', 'CollegeController@update');
         $api->get('delete_colleges/{college_id}', 'CollegeController@delete');
