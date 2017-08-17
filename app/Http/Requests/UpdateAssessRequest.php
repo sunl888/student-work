571bc107aa89007d6cc33ce5ccef7a869c1e7c5c
sunlong <2013855675@qq.com>
2017-08-17 11:51:09 +0800

    fix bug

--- app/Http/Requests/UpdateAssessRequest.php
+++ app/Http/Requests/UpdateAssessRequest.php
@@ -24,15 +24,16 @@ class UpdateAssessRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' =>'required',
-            'score' =>'required|numeric|between:1,100',
+            'title' =>'string',
+            'score' =>'numeric|between:1,100',
         ];
     }
 
     public function messages(){
         return [
-            'title.required' =>'考核等级必须要填写',
-            'score.required' =>'该等级必须添加预制评分',
+            //'title.required' =>'考核等级必须要填写',
+            'title.string' =>'考核等级必须是字符串',
+            //'score.required' =>'该等级必须添加预制评分',
             'score.numeric' =>'评分必须是数值型',
             'score.between' =>'评分范围是1~100之间'
         ];
