883651034a40e1ed2a9f381d1b7ccee74cd65642
wqer1019 <2013855675@qq.com>
2017-09-24 17:53:09 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,16 +6,6 @@ namespace App\Http\Controllers;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    /*public function __construct()
-    {
-        $this->middleware('auth');
-    }*/
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
