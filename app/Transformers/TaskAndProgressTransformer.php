f5878db19c7ab1812cae287b3cad36df4a4e4128
wqer1019 <2013855675@qq.com>
2017-09-26 00:20:37 +0800

    fix bug

--- app/Transformers/TaskAndProgressTransformer.php
+++ app/Transformers/TaskAndProgressTransformer.php
@@ -31,7 +31,8 @@ class TaskAndProgressTransformer extends Transformer
             //完成状态
             'finished_at' => $task->task_progress->status,
             //责任人
-            'user' => $task->task_progress->user_id == TaskProgress::$personnelSign ? '全体人员' : app(User::class)->find($task->task_progress->user_id)['name'],
+            'user' => isset($task->task_progress->user_id) ? $this->getUser($task->task_progress->user_id):null,
+            //'user' => $task->task_progress->user_id == TaskProgress::$personnelSign ? '全体人员' : app(User::class)->find($task->task_progress->user_id)['name'],
             //等级
             'assess' => !empty($task->task_progress->assess_id) ? app(AssessRepository::class)->find($task->task_progress->assess_id)['title'] : null,
             //完成质量
@@ -41,6 +42,22 @@ class TaskAndProgressTransformer extends Transformer
             //推迟理由
             'delay' => $task->task_progress->delay,
         ];
+    }
+
+    public function getUser($users)
+    {
+        $userIds = explode(',', $users);
+        if (array_first($userIds) != null) {
+            if (strtolower(array_first($userIds)) == TaskProgress::$personnelSign) {
+                return ['name'=>'全体人员'];
+            } elseif (count($userIds) == 1) {
+                return User::find(array_first($userIds), ['id', 'name']);
+            } elseif (count($userIds) > 1) {
+                return User::whereIn('id', $userIds)->get(['id', 'name']);
+            }
+        } else {
+            return null;
+        }
     }
 
 }
