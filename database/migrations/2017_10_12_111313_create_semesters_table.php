b5f224d3396e3e0c7f174f4a6399fdb40cb42c01
wqer1019 <2013855675@qq.com>
2017-10-12 11:18:35 +0800

    add settings

---
+++ database/migrations/2017_10_12_111313_create_semesters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSemestersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title'); //2016-2017
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->unsignedInteger('parent_id')->default(0);// 0->学年
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semesters');
+    }
+}
