67ed90d9b0790d6e74319d24e907189b51dc7356
zyq666 <youare338@163.com>
2017-12-11 16:11:24 +0800

    引用本地图标

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="renderer" content="webkit"> 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="icon" href="../../favicon.ico" type="image/x-icon">
     <title>淮南师范学院学生事务进程考核系统</title>
     <style type="text/css">
@@ -31,6 +30,40 @@
         .link:hover{
             color: #20a0ff;
             text-decoration: underline;
+        }
+        @font-face {
+            font-family: 'Material Icons';
+            font-style: normal;
+            font-weight: 400;
+            src: url(../../fonts/iconfont/MaterialIcons-Regular.eot); /* For IE6-8 */
+            src: local('Material Icons'),
+            local('MaterialIcons-Regular'),
+            url(../../fonts/iconfont/MaterialIcons-Regular.woff2) format('woff2'),
+            url(../../fonts/iconfont/MaterialIcons-Regular.woff) format('woff'),
+            url(../../fonts/iconfont/MaterialIcons-Regular.ttf) format('truetype');
+        }
+
+        .material-icons {
+            font-family: 'Material Icons';
+            font-weight: normal;
+            font-style: normal;
+            font-size: 24px; /* Preferred icon size */
+            display: inline-block;
+            width: 1em;
+            height: 1em;
+            line-height: 1;
+            text-transform: none;
+
+            /* Support for all WebKit browsers. */
+            -webkit-font-smoothing: antialiased;
+            /* Support for Safari and Chrome. */
+            text-rendering: optimizeLegibility;
+
+            /* Support for Firefox. */
+            -moz-osx-font-smoothing: grayscale;
+
+            /* Support for IE. */
+            font-feature-settings: 'liga';
         }
     </style>
 
